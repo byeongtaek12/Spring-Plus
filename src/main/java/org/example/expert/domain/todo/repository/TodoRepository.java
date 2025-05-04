96afaf9e9d564fcdfa204642ee075f8872188924
obt <byeongtaek12@naver.com>
2025-05-04 21:09:29 +0900

    LV1: 3. 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user u " +
+            "WHERE (t.weather = :weather OR :weather IS NULL) AND " +
+            "((:startDate IS NULL AND :endDate IS NULL) OR" +
+            " (t.modifiedAt BETWEEN COALESCE(:startDate, t.modifiedAt) AND" +
+            " COALESCE(:endDate, t.modifiedAt))) ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByWeatherAndPeriodRangeOrderByModifiedAtDesc(Pageable pageable,
+                                              @Param("weather") String weather,
+                                              @Param("startDate")LocalDateTime startDate,
+                                              @Param("endDate") LocalDateTime endDate);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
